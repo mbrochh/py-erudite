ae048a28bcc023b05f896c7aedc9dc6e6bf37c1b
Martin Brochhaus <mbrochh@gmail.com>
2024-02-21 22:45:47 +0800

    feat: renames command from run_ingest to just ingest / improves stdout output

--- backend/pyerudite/ingest/management/commands/ingest.py
+++ backend/pyerudite/ingest/management/commands/ingest.py
@@ -6,6 +6,8 @@ Management command to work off IngestFromSource objects with status `pending`.
 import os
 
 from django.core.management.base import BaseCommand
+
+from pyerudite.utils import timestamp
 from ingest.models import IngestFromSource
 from ingest.services import transcribe_video
 from summarize.models import SummarizeFromIngest
@@ -14,7 +16,7 @@ from summarize.models import SummarizeFromIngest
 class Command(BaseCommand):
     help = "Works off IngestFromSource objects with status `pending`."
     lock_file_path = "/artefacts/tmp/"
-    lock_file_name = "run_ingest.lock"
+    lock_file_name = "ingest.lock"
 
     def handle(self, *args, **options):
         """
@@ -28,7 +30,9 @@ class Command(BaseCommand):
 
         if os.path.exists(lockfile_path):
             self.stdout.write(
-                self.style.ERROR(f"Lock file `{lockfile_path}` exists.")
+                self.style.WARNING(
+                    f"[{timestamp()}] Lock file `{lockfile_path}` exists."
+                )
             )
             return
 
@@ -42,12 +46,24 @@ class Command(BaseCommand):
             os.remove(lockfile_path)
             self.stdout.write(
                 self.style.SUCCESS(
-                    f"Lock file `{lockfile_path}` removed."
+                    f"[{timestamp()}] Lock file `{lockfile_path}` removed."
                     " Process completed."
                 )
             )
 
     def _handle(self):
         jobs = IngestFromSource.objects.filter(status="pending")
+        self.stdout.write(
+            self.style.NOTICE(
+                f"[{timestamp()}] Found {jobs.count()} IngestFromSource objects"
+                " with status `pending`."
+            )
+        )
         for job in jobs:
+            self.stdout.write(
+                self.style.NOTICE(
+                    f"[{timestamp()}] Processing IngestFromSource ({job.id})"
+                    f" for source {job.source_url}."
+                )
+            )
             transcribe_video(ingest_obj=job)
