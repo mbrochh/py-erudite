4e31c44a2dca37079b5e2d0bcf679fd799823e8c
Martin Brochhaus <mbrochh@gmail.com>
2024-02-21 22:47:58 +0800

    feat: installs cron & vim into backend container / adds crontab.txt / makes sure that crontab is set when container is built

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -3,14 +3,20 @@
 # Wait a bit for the mounted volume to be available
 sleep 2
 
+mkdir -p /artefacts/logs/backend/cron/
+
 # Navigate to the Django project directory (if necessary)
 cd /app/pyerudite
+
 
 # Apply database migrations
 python manage.py migrate
 
 # Collect static files
 python manage.py collectstatic --noinput
+
+# Start the cron service
+service cron start
 
 # Start the Django development server
 python manage.py runserver 0.0.0.0:8000
