512dbdcf672ce0e26508e16a27942d8d7961f0f6
Martin Brochhaus <mbrochh@gmail.com>
2024-02-24 15:01:24 +0800

    feat: changed port for Django admin to 4242 / removing lockfiles when container starts

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -5,6 +5,10 @@ sleep 2
 
 mkdir -p /artefacts/logs/backend/cron/
 
+# Remove lockfiles from earlier runs
+rm /artefacts/tmp/ingest.lock
+rm /artefacts/tmp/summarize.lock
+
 # Navigate to the Django project directory (if necessary)
 cd /app/pyerudite
 
@@ -19,4 +23,4 @@ python manage.py collectstatic --noinput
 service cron start
 
 # Start the Django development server
-python manage.py runserver 0.0.0.0:8000
+python manage.py runserver 0.0.0.0:4242
